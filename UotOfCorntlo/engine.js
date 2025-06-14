22df7c9506fbfb4a318065cb9fc1cb2f0cb17985
prog255 <raikala255@gmail.com>
2025-06-14 13:18:17 +0300

    fix timer

--- UotOfCorntlo/engine.js
+++ UotOfCorntlo/engine.js
@@ -120,7 +120,7 @@ document.addEventListener('keydown', function(event) {
 });
 document.addEventListener('keyup', function(event) {
 	if (!started && event.keyCode == 16) {
-		startRender(); this.style.display = 'none';
+		startRender(); document.getElementById("start").style.display = 'none';
 		return;
 	}
 	if (moments[currentMoment].onkey["u" + event.keyCode] !== undefined) {
