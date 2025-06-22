39ca7bfa7ca960b00632465d8abffaa2cdc1ed32
prog255 <raikala255@gmail.com>
2025-06-22 20:22:40 +0300

    Add files via upload

---
+++ kiigeapp/js.js
@@ -0,0 +1,22 @@
+var degree, doneSaying = false, roundmax = 0, overallmax;
+
+const acl = new Accelerometer({ frequency: 100 });
+acl.addEventListener("reading", () => {
+	degree = acl.x * 10;
+	if (degree > max) {
+		max = degree;
+	}
+	if (degree > overallmax) {
+		overallmax = degree;
+	}
+	if (degree < 10 && !doneSaying) {
+		document.write("done");
+		doneSaying = true;
+		max = 0;
+	}
+	if (degree > 20) {
+		doneSaying = false;
+	}
+});
+
+acl.start();
