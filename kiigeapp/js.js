c4bbe532c3cd7a11ff60b874be42bc6413f91d10
pg255 pg255 <raikala255@gmail.com>
2025-06-23 11:23:16 +0300

    update

--- kiigeapp/js.js
+++ kiigeapp/js.js
@@ -1,12 +1,17 @@
-var hasSaid = false;
+var hasSaid = false, record, highest;
 
 function onRotate() {
-	document.getElementById("degree").innerHTML = rotX;
+	document.getElementById("degree").innerHTML = rotX.toFixed(2);
+	document.getElementById("degree").innerHTML = record.toFixed(2);
 	if (rotX < 10 && !hasSaid) {
 		hasSaid = true;
 	}
 	if (20 < rotX) {
 		hasSaid = false;
+	}
+
+	if (rotX > highest) {
+		highest = rotX;
 	}
 
 	oscillator.frequency.setValueAtTime(parseFloat(document.getElementById("0pitch").value) + Math.abs(rotX * parseFloat(document.getElementById("multiplier").value)), audioCtx.currentTime);
